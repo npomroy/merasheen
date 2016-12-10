5c01dd14852e52a00b9154811d78a8d16389abb2
Nick Pomroy <npomroy69@hotmail.com>
2016-12-10 08:26:15 +0000

    Added boat create action

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,5 +1,23 @@
 class BoatsController < ApplicationController
+    # GET /boats/new
     def new
-        
+        @boat = Boat.new
     end
+    
+    # POST /boats
+    def create
+        @boat = Boat.new( boat_params )
+        if @boat.save
+            flash[:success] = "Boat created"
+            redirect_to new_boat_path
+        else
+            flash[:error] = "Creation failed"
+            redirect_to new_boat_path
+        end
+    end
+    
+    private
+        def boat_params
+           params.require(:boat).permit(:name, :owner, :home_port, :capacity, :price, :speed, :comments) 
+        end
 end
