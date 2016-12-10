983bc069c903538c5ebaae576c583e1b5480582e
Nick Pomroy <npomroy69@hotmail.com>
2016-12-10 17:28:32 +0000

    Added trip create action

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,5 +1,22 @@
 class TripsController < ApplicationController
     def new
-       
+       @trip = Trip.new
     end
+    
+    # POST /trips
+    def create
+       @trip = Trip.new( trip_params )
+       if @trip.save
+           flash[:success] = "Trip created"
+           redirect_to new_trip_path
+       else
+           flash[:error] = "Trip Creation Failed"
+           redirect_to new_trip_path
+       end
+    end
+    
+    private
+        def trip_params
+           params.require(:trip).permit(:name, :depart_from, :depart_date_time, :boat_id, :skipper, :available_seats, :starting_price, :comments) 
+        end
 end
