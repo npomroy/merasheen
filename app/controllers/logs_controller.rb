dc9bea23b3372cfe878da29c08eb044ae7bf9070
Nick Pomroy <npomroy69@hotmail.com>
2016-12-18 13:37:22 +0000

    Added log create action

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -2,4 +2,22 @@ class LogsController < ApplicationController
    def new
        @log = Log.new
    end
+   
+   def create
+      @log = Log.new( log_params )
+      @log.user_id = current_user.id
+      @log.likes = 0
+      if @log.save
+          flash[:success] = "Log added"
+          redirect_to root_path
+      else
+          flash[:error] = 'Log creation failed'
+          render action: :new
+      end
+   end
+   
+   private
+        def log_params
+            params.require(:log).permit(:title, :user_id, :link, :details, :startdate, :enddate, :likes)
+        end
 end
