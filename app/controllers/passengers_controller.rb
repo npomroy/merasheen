7a0cf1d11e56f3213067ae0d807ba57344602f7e
Nick Pomroy <npomroy69@hotmail.com>
2016-12-18 12:01:57 +0000

    Added passegner create action

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -2,4 +2,21 @@ class PassengersController < ApplicationController
    def new
       @passenger = Passenger.new 
    end
+   
+   def create
+      @passenger = Passenger.new(passenger_params)
+      @passenger.user_id = current_user.id
+      if @passenger.save
+          flash[:success] = "Passenger created"
+          redirect_to root_path
+      else
+          flash[:error] = "Passenger creation failed"
+          render action: :new
+      end
+   end
+   
+   private
+        def passenger_params
+           params.require(:passenger).permit(:title, :earliestdate, :latestdate, :details, :user_id) 
+        end
 end
