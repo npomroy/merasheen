fe6ad43752d67269ae398c67707e30c5e5da6bda
Nick Pomroy <npomroy69@hotmail.com>
2016-12-18 15:19:12 +0000

    Added storie create action

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,5 +1,22 @@
 class StoriesController < ApplicationController
    def new
-      @storie = Storie.new 
+      @story = Storie.new 
    end
+   
+   def create
+      @story = Storie.new(story_params)
+      @story.user_id = current_user.id
+      if @story.save
+          flash[:success] = "Story created"
+          redirect_to root_path
+      else
+          flash[:error] = "Story creation failed"
+          redner action: :new
+      end
+   end
+   
+   private
+        def story_params
+            params.require(:storie).permit(:user_id, :title, :location, :date, :description)
+        end
 end
