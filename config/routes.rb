edc6b4579b7e13cd6ccb7e0cc202eb167f6d85e9
Nick Pomroy <npomroy69@hotmail.com>
2016-12-18 12:55:30 +0000

    Added all aboard view and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'about', to: 'pages#about'
+  get 'all_aboard', to: 'pages#all_aboard'
   resources :users do
     resource :profile
   end
