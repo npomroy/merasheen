c30dbb3910c35a8d706197b792f89d66baecaf56
Nick Pomroy <npomroy69@hotmail.com>
2016-12-19 17:35:27 +0000

    Added application create action

--- app/controllers/trip_applications_controller.rb
+++ app/controllers/trip_applications_controller.rb
@@ -2,4 +2,20 @@ class TripApplicationsController < ApplicationController
    def new
        @application = TripApplication.new
    end
+   
+   def create
+      @application = TripApplication.new( application_params )
+      if @application.save
+          flash[:success] = "Application submitted"
+          redirect_to trips_path
+      else
+          flash[:error] = "Application failed"
+          render action: :new
+      end
+   end
+   
+   private
+        def application_params
+           params.require(:trip_application).permit(:seat_total, :max_price_per_seat, :trip_id) 
+        end
 end
