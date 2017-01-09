61aae26e74cd893df689d489f9ed861a7ffa8592
Nick Pomroy <npomroy69@hotmail.com>
2017-01-09 01:06:53 +0000

    Added user_id to trips table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161219163340) do
+ActiveRecord::Schema.define(version: 20170109010532) do
 
   create_table "boats", force: :cascade do |t|
     t.string   "name"
@@ -101,6 +101,7 @@ ActiveRecord::Schema.define(version: 20161219163340) do
     t.text     "comments"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.integer  "user_id"
   end
 
   create_table "trips_profiles", id: false, force: :cascade do |t|
