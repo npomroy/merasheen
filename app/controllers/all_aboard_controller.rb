3d94248e520feef29fdc75959313153e1e2fd6bf
Nick Pomroy <npomroy69@hotmail.com>
2017-01-11 04:32:11 +0000

    Added applications to applications view

--- app/controllers/all_aboard_controller.rb
+++ app/controllers/all_aboard_controller.rb
@@ -1,5 +1,6 @@
 class AllAboardController < ApplicationController
    def applications
-       
+       @myapplications = TripApplication.where("user_id = ?", current_user.id)
+       @tripapplications = TripApplication.where("owner_id = ?", current_user.id)
    end
 end
